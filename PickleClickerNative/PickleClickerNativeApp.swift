6b140c9917f4aba7eb9938eac60e17877a7ce3fd
Dillon Rodgers <dillonrodgers73@gmail.com>
2026-04-06 12:36:22 -0500

    Initial Commit

---
+++ PickleClickerNative/PickleClickerNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickleClickerNativeApp.swift
+//  PickleClickerNative
+//
+//  Created by Dillon Rodgers on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct PickleClickerNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
